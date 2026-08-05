4a93c0e7ad8775d8d68ebfd7a69448bc14b2e19e
Krish Sonmanik <krishsonmanik.0906@gmail.com>
2026-08-05 21:05:40 +0530

    Time: 2 ms (99.31%), Space: 44.4 MB (64.71%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,15 +1,22 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        String str="";
-        s = s.toLowerCase();
-        for (int i =0; i<s.length(); i++){
-            if (((int)s.charAt(i) >= 97 && (int)s.charAt(i)<=122) || ((int)s.charAt(i) >= 48 && (int)s.charAt(i)<=57 ))
-            str = str+ s.charAt(i);
-        }
-
-        for (int j =0; j<str.length()/2; j++){
-            if (str.charAt(j) != str.charAt(str.length()-1-j))
-            return false;
+        int start = 0, last = s.length() - 1;
+        while(start <= last) {
+        	char currFirst = s.charAt(start);
+        	char currLast = s.charAt(last);
+        	if (!Character.isLetterOrDigit(currFirst )) {
+        		start++;
+        	} 
+            else if(!Character.isLetterOrDigit(currLast)) {
+        		last--;
+        	} 
+            else {
+        		if (Character.toLowerCase(currFirst) != Character.toLowerCase(currLast)) {
+        			return false;
+        		}
+        		start++;
+        		last--;
+        	}
         }
         return true;
     }
