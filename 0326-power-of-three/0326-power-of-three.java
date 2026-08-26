370e7f1e58eab92c6846eaf0b8533627c3b03c37
Krish Sonmanik <krishsonmanik.0906@gmail.com>
2026-08-26 23:14:03 +0530

    Time: 8 ms (92.81%), Space: 46.3 MB (10.67%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.java
+++ 0326-power-of-three/0326-power-of-three.java
@@ -1,6 +1,6 @@
 public class Solution {
     public boolean isPowerOfThree(int n) {
-        int maxPowerOf3 = 1162261467; // 3^19 is the largest power of 3 in int range
+        int maxPowerOf3 = 1162261467; 
         return n > 0 && maxPowerOf3 % n == 0;
     }
 }
