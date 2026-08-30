082ebe91dc19e53ed1eb25f8334cad0f4d7225f3
Krish Sonmanik <krishsonmanik.0906@gmail.com>
2026-08-31 01:26:54 +0530

    Time: 0 ms (100%), Space: 43.1 MB (53.71%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.java
+++ 0844-backspace-string-compare/0844-backspace-string-compare.java
@@ -1,39 +1,24 @@
 class Solution {
-    public boolean backspaceCompare(String s, String t) {
-        Stack<Character> c1 = new Stack<>();
-        Stack<Character> c2 = new Stack<>();
-        
-        int l1=0, l2=0;
-        for (int i = 0; i <s.length(); i++){
-            if (s.charAt(i) == '#' && c1.isEmpty())
-            continue;
-            if (s.charAt(i) == '#' && !c1.isEmpty())
-            c1.pop();
-            else
-            c1.push(s.charAt(i));
-        }
-
-        for (int j = 0;  j<t.length(); j++){
-            if (t.charAt(j) == '#' && c2.isEmpty())
-            continue;
-            if (t.charAt(j) == '#' && !c2.isEmpty())
-            c2.pop();
-            else
-            c2.push(t.charAt(j));
-        }
-
-        while (!c1.isEmpty() && !c2.isEmpty()){
-            if (c1.peek() != c2.peek())
-            return false;
-            else{
-                c1.pop();
-                c2.pop();
+    public boolean backspaceCompare(String S, String T) {
+        int i = S.length() - 1, j = T.length() - 1, back;
+        while (true) {
+            back = 0;
+            while (i >= 0 && (back > 0 || S.charAt(i) == '#')) {
+                back += S.charAt(i) == '#' ? 1 : -1;
+                i--;
+            }
+            back = 0;
+            while (j >= 0 && (back > 0 || T.charAt(j) == '#')) {
+                back += T.charAt(j) == '#' ? 1 : -1;
+                j--;
+            }
+            if (i >= 0 && j >= 0 && S.charAt(i) == T.charAt(j)) {
+                i--;
+                j--;
+            } else {
+                break;
             }
         }
-
-        if (c1.isEmpty() && c2.isEmpty())
-        return true;
-        else
-        return false;
+        return i == -1 && j == -1;
     }
 }
