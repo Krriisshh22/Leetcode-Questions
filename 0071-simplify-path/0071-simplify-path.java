35b6911e1cb6a3ba999ba1f91198cd984886ab9e
Krish Sonmanik <krishsonmanik.0906@gmail.com>
2026-09-01 23:06:37 +0530

    Time: 6 ms (28.81%), Space: 47.7 MB (5.89%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.java
+++ 0071-simplify-path/0071-simplify-path.java
@@ -50,10 +50,6 @@ class Solution {
 
         str = concat(s, str);
 
-        // String str2 ="";
-        // for (int i = str.length()-1; i>=0; i--){
-        //     str2 += str.charAt(i);
-        // }
         return str;
     }
 }
