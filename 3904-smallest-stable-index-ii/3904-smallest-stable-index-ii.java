a42813af91d720eb083c4a0922d3484d90b1aca5
Krish Sonmanik <krishsonmanik.0906@gmail.com>
2026-09-05 10:07:11 +0530

    Time: 6 ms (45.8%), Space: 126.7 MB (71.37%) - LeetHub

--- 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.java
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.java
@@ -1,25 +1,22 @@
 class Solution {
     public int firstStableIndex(int[] nums, int k) {
-        Stack<Integer> s = new Stack<>();
-        int n = nums.length-1;
-        s.push(n);
-        for (int i =n-1; i>=0; i--){
-            if (nums[i]<nums[s.peek()])
-            s.push(i);
+        int n = nums.length;
+
+        int max[] = new int[n];
+        int min[] = new int[n];
+
+        max[0] = nums[0];
+        for (int i =1; i<n; i++){
+            max[i] = Math.max(max[i-1], nums[i]);
         }
-        int max = nums[0];
-        int minI = s.peek();
 
-        for (int x =0; x<nums.length; x++){
-            if (x>minI){
-                s.pop();
-                if (!s.empty())
-                minI = s.peek();
-            }
+        min[n-1] = nums[n-1];
+        for (int j = n-2; j>=0; j--){
+            min[j] = Math.min(min[j+1], nums[j]);
+        }
 
-            max = Math.max (max, nums[x]);
-
-            if (max-nums[minI] <= k)
+        for (int x =0; x<n; x++){
+            if (max[x] - min[x] <= k)
             return x;
         }
         return -1;
