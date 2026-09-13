7ae17cebfafa757868b6a70e75ffc47bfc8bfe23
Krish Sonmanik <krishsonmanik.0906@gmail.com>
2026-09-13 19:29:34 +0530

    Time: 2 ms (97.49%), Space: 72.4 MB (69.11%) - LeetHub

---
+++ 0860-lemonade-change/0860-lemonade-change.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+
+        int fives =0, tens = 0;
+        for (int i =0; i<bills.length; i++){
+            if (bills[i] == 5)
+                fives++;
+            else if (bills[i] == 10){
+                fives--;
+                tens++;
+            }
+            else{
+                if (tens>0){
+                    fives--;
+                    tens--;
+                }
+                else{
+                    fives -=3;
+                }
+            }
+            if (fives<0 || tens<0)
+                return false;
+        }
+        return true;
+    }
+}
